beca7413fb8452b4bb8a43bcfab0f7c861c896e3
Austin637 <austinesparza@gmail.com>
2019-03-18 20:13:53 -0700

    Comments.css and added global styles

--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -1,23 +1,35 @@
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { fab } from '@fortawesome/free-brands-svg-icons';
-import { faInstagram } from '@fortawesome/free-brands-svg-icons';
-import { faCompass, faHeart, faUser } from '@fortawesome/free-regular-svg-icons';
-import './SearchBar.css';
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faInstagram } from "@fortawesome/free-brands-svg-icons";
+import {
+  faCompass,
+  faHeart,
+  faUser
+} from "@fortawesome/free-regular-svg-icons";
+import "./SearchBar.css";
 
-import Logo from '../../img/instagram-logo.png';
+import Logo from "../../img/instagram-logo.png";
 const SearchBar = () => {
-    return (
-        <div className="search-bar">
-            <FontAwesomeIcon icon={faInstagram} />
-            <img className="ig-logo" alt="Instagram logo" src={Logo} />
-            <input type="text" />
-            <FontAwesomeIcon icon={faCompass} />
-            <FontAwesomeIcon icon={faHeart} />
-            <FontAwesomeIcon icon={faUser} />
+  return (
+    <div className="search-bar">
+      <div className="search-logo-container">
+        <div className="ig-icon-container">
+          <FontAwesomeIcon icon={faInstagram} size="3x" />
         </div>
-    );
-}
 
+        <img className="ig-logo" alt="Instagram logo" src={Logo} />
+      </div>
+
+      <div className="search-input-container" placeholder="Search">
+        <input type="text" />
+      </div>
+      <div className="search-icons-container">
+        <FontAwesomeIcon size="2x" icon={faCompass} />
+        <FontAwesomeIcon size="2x" icon={faHeart} />
+        <FontAwesomeIcon size="2x" icon={faUser} />
+      </div>
+    </div>
+  );
+};
 
 export default SearchBar;
