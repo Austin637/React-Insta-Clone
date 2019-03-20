36b60af62f06989239537eb3fe9d972b07c760d9
Austin637 <austinesparza@gmail.com>
2019-03-20 00:09:24 -0700

    Login component

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -2,6 +2,7 @@ import React, {
   Component
 } from 'react';
 import PostsPage from './components/PostContainer/PostsPage';
+import authenticate from './components/authentication/authenticate';
 import './App.css';
 
 class App extends Component {
